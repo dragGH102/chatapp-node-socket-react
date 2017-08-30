fc98ba38cb24031b7e596f9dd480edc164ce79d2
Manuel-Gajo <manuelgajo.mnl@gmail.com>
2017-08-30 17:10:20 +0200

    socket.io server logic (w.i.p.)

--- server.js
+++ server.js
@@ -1,39 +1,43 @@
-const express = require('express');
+const express = require('express')();
+const server = require('http').Server(express);
+const io = require('socket.io')(server);
 const next = require('next');
-const http = require('http');
-const io = require('socket.io')(http);
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
 const args = require('optimist').argv;
 const port = args.p || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
 
-app.prepare()
+
+// socket.io
+io.on('connection', (socket) => {
+    // user connected
+    console.log('connected');
+
+    // send to the other clients
+    socket.broadcast.emit('connected');
+
+    socket.on('disconnect', () => {
+        console.log('disconnected');
+        socket.broadcast.emit('disconnected');
+    });
+});
+
+nextApp.prepare()
   .then(() => {
-    const server = express();
-
-    server.get('*', (req, res) => {
+    // express server set up
+    express.get('*', (req, res) => {
       return handle(req, res)
     });
 
-    server.listen(port, (err) => {
-      if (err) throw err;
-      console.log(`> Ready on http://localhost:${port}`);
+    express.get('/', (req, res) => {
+      res.end();
     });
 
-    // socket.io
-    io.on('connection', (socket) => {
-        // user connected
-        console.log('connected');
-
-        // send to the other clients
-        socket.broadcast.emit('connected');
-
-        socket.on('disconnect', () => {
-            console.log('disconnected');
-            socket.broadcast.emit('disconnected');
-        });
+    express.listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`);
     });
   })
   .catch((ex) => {
