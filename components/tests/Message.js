95babc8830c63ee180fbddd5dda263e18aa1b225
Manuel-Gajo <manuelgajo.mnl@gmail.com>
2017-08-31 20:07:20 +0200

    Message snapshot test

--- components/tests/Message.js
+++
@@ -1,7 +0,0 @@
-// a similar test (snapshot test) can be written for the other components
-
-describe('<Message />', () => {
-    it('should return correctly"', () => {
-       // TODO (snapshot)
-    });
-});
---
+++ components/tests/Message.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Message from "../Message";
+import testUtils from "../../utils/tests";
+
+// a similar test (snapshot test) can be written for the other components
+describe('<Message />', () => {
+    it('should render correctly"', () => {
+        const message = testUtils.generateMessage('me');
+        
+        const component = renderer.create(<Message message={message} />);
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
