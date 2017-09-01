ed8a3e5d06b82980328936390a33f4515e48e3d8
Manuel-Gajo <manuelgajo.mnl@gmail.com>
2017-09-01 14:29:43 +0200

    test for EMOTICON

--- components/tests/Message.test.js
+++ components/tests/Message.test.js
@@ -2,14 +2,24 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import Message from "../Message";
 import { generateMessage } from "../../utils/tests";
+import { mount } from "enzyme";
+import Emoticon from "../Emoticon";
 
-// a similar test (snapshot test) can be written for the other components
 describe('<Message />', () => {
     it('should render correctly"', () => {
+        // a similar test (snapshot test) can be written for the other components
         const message = generateMessage('me');
 
         const component = renderer.create(<Message message={message} />);
         const tree = component.toJSON();
         expect(tree).toMatchSnapshot();
+    });
+
+    it('should include EMOTICON component if valid emoticon string provided', () => {
+        const message = generateMessage('me');
+        message.content = 'Hey, this is a (smile)! ... but now I am (frown)';
+
+        const component = mount(<Message message={message} />);
+        expect(component.find(Emoticon)).toHaveLength(2);
     });
 });
